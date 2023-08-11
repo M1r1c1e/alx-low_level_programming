d54b11fc504294c845052c4e1cad3a0ee7de9f0b
M1r1c1e <andersomiracle111@gmail.com>
2023-08-10 20:14:05 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,9 @@
 
 int main(void) 
 {
+	/* Use the puts function to print the desired message */
 	puts("Programming is like building a multilingual puzzle");
+	/* Return 0 to indicate successful program execution */
 	return 0;
 }
 
