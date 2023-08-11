2c40cfa224f09ff9ae535e9910284cddd1318029
M1r1c1e <andersomiracle111@gmail.com>
2023-08-10 20:42:54 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/* Use the printf function to print the desired message */
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+
+	/* Return 0 to indicate successful program execution */
+	return o;
+}
