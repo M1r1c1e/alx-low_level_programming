25c063fe7133b1a7274c21be655bb4a0aa8b03dc
M1r1c1e <andersomiracle111@gmail.com>
2023-08-11 03:39:27 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #!/bin/bash
 #include <stdio.h>
-
-int main(void) 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
 {
-	/* Use the puts function to print the desired message */
-	puts("Programming is like building a multilingual puzzle");
-	/* Return 0 to indicate successful program execution */
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
