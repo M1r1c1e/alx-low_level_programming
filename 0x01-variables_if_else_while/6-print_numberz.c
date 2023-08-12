e59ab28282d6d439d136bd4c46a5e91e29df8262
M1r1c1e <andersomiracle111@gmail.com>
2023-08-12 15:45:04 -0700

    printing numb

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int num = 0
+
+	while (num < 10)
+
+	{
+	putchar(num + '0');
+
+	num++;
+	}
+
+	putchar('\n');
+
+	return (0);
