30098e37cdfa0038908b917b71e827aa4a35e4fc
M1r1c1e <andersomiracle111@gmail.com>
2023-08-12 14:21:57 -0700

    alphabet omission

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char j;
+
+	for (j = 'a'; j <= 'z'; j++)
+
+	{
+	if (j != 'e' && j != 'q')
+	putchar(j);
+	}
+
+	putchar('\n');
+
+	return (0);
