893c536e7324284e940f8cbd8156b4630fa0aea8
M1r1c1e <andersomiracle111@gmail.com>
2023-08-11 22:48:10 -0700

    if or else if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 
 /* betty style doc for function main goes there */
 /**
- * main - Entry point
+ * main - main function
  *
  * Return: Always 0 (Success)
  */
@@ -23,6 +23,6 @@ int main(void)
 		printf("%d is zero\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
-	printf("\n")
+	printf("\n");
 	return (0);
 }
