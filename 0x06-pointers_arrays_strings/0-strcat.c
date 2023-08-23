a5a5464ca0434e4158a7fdfb5976bcd22ff4f4cf
M1r1c1e <andersomiracle111@gmail.com>
2023-08-23 15:37:35 -0700

    concantenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenating two strings
+ * @dest: destination
+ * @src: source
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char* real_dest = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+
+	return (real_dest);
+}
