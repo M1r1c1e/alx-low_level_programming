f0c868f2bf454b976c7b0095f6992c062e6d9595
M1r1c1e <andersomiracle111@gmail.com>
2023-09-05 05:35:57 -0700

    concantenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,62 +12,40 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *fall;
-
 	int mg, sg;
 
 	if (s1 == NULL)
-
 		s1 = "";
 
 	if (s2 == NULL)
-
 		s2 = "";
-
-
 	mg = sg = 0;
 
 	while (s1[mg] != '\0')
-
 		mg++;
 
 	while (s2[sg] != '\0')
-
 		sg++;
 
 	fall = malloc(sizeof(char) * (mg + sg + 1));
 
 
 	if (fall == NULL)
-
 		return (NULL);
-
 	mg = sg = 0;
 
 	while (s1[mg] != '\0')
-
 	{
-
 		fall[mg] = s1[mg];
-
 		mg++;
-
 	}
-
-
 	while (s2[sg] != '\0')
-
 	{
-
 		fall[mg] = s2[sg];
-
 		mg++, sg++;
 
 	}
-
 	fall[mg] = '\0';
-
 	return (fall);
 
 }
-
-
