efaa10c40db7a495c1f83e8e7774daa739008f67
M1r1c1e <andersonmiracle111@.com>
2024-04-09 09:09:46 -0700

    search algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -16,10 +16,10 @@
  */
 typedef struct listint_s
 {
-    int n;
-    size_t index;
-    struct listint_s *next;
-} listint_t;
+	int n;
+	size_t index;
+	struct listint_s *next;
+}	listint_t;
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
